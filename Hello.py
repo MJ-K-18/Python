828c19e29cc347b069d171cea316a3fe2ef90359
MJ-K-18 <touya18@naver.com>
2019-07-02 10:42:51 +0900

    Initial commit

---
+++ Hello.py
@@ -0,0 +1,16 @@
+'''
+   Hello.py
+   문자열 주석
+'''
+def hello():
+    print( 'Hello, World' )
+
+    sum = 1 + 2 + \
+        3+ 4 + \
+        5 + 6 + \
+        7 + 8 + \
+        9 + 10
+    print( sum )
+    print( 'hello function( ) - finish' )
+if __name__ =='__main__':
+    hello()
